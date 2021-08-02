386e4af65a88dab850911cc3afc8ec5863d035d2
nicholas <JVListic@gmail.com>
2021-08-02 14:10:35 +0800

    add map screeb

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MapScreen from "./screens/MapScreen";
 
-// 1. Set up Redux
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
 		<Provider store={store}>
-			<SafeAreaProvider>
-				<HomeScreen />
-			</SafeAreaProvider>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<Stack.Navigator>
+						<Stack.Screen
+							name="HomeScreen"
+							component={HomeScreen}
+							options={{
+								headerShown: false,
+							}}
+						/>
+						<Stack.Screen
+							name="MapScreen"
+							component={MapScreen}
+							options={{
+								headerShown: false,
+							}}
+						/>
+					</Stack.Navigator>
+				</SafeAreaProvider>
+			</NavigationContainer>
 		</Provider>
 	);
 }
